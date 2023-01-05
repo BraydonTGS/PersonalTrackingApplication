e76d7d9a26f07d2c83b85736f3be550dcddd8824
Braydon Sutherland <braydonsutherland@gmail.com>
2023-01-05 14:09:17 -0600

    Personal Tracking Management Application Created the PersonalTracking.WPF project

---
+++ PersonalTracking.WPF/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace PersonalTracking.WPF; 
+
+public partial class App : Application
+{
+}
---
+++ PersonalTracking.WPF/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace PersonalTracking.WPF; 
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
