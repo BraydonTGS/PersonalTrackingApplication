2e33643222123f9bf2bdff12f06daea246d27f2a
Braydon Sutherland <braydonsutherland@gmail.com>
2023-01-06 11:04:43 -0600

    Department Page Can Add a new Department to the DataBase

--- PersonalTracking.WPF/Views/DepartmentList.xaml.cs
+++ PersonalTracking.WPF/Views/DepartmentList.xaml.cs
@@ -7,15 +7,22 @@ namespace PersonalTracking.WPF.Views
 {
     public partial class DepartmentList : UserControl
     {
+        private readonly PersonalTrackingDbContext _context; 
         public DepartmentList()
         {
             InitializeComponent();
-            using(PersonalTrackingDbContext context= new PersonalTrackingDbContext()) 
-            {
-                var departments = context.Departments.ToList(); 
-                gridDepartment.ItemsSource= departments;
-            }
-          
+            PersonalTrackingDbContext context = new PersonalTrackingDbContext();
+            _context= context;
+            gridDepartment.ItemsSource = _context.Departments.ToList(); 
+        
+        }
+
+        private void btnAdd_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DepartmentPage departmentPage = new DepartmentPage();
+            departmentPage.ShowDialog();
+            gridDepartment.ItemsSource = _context.Departments.ToList(); 
+
         }
     }
 }
